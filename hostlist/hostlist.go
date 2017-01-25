882a53511338bac81d5159ccf2984b4aa3912ff6
Joe <joelillo113@gmail.com>
2017-01-24 22:34:24 -0700

    Comments in hostlists.

--- hostlist/hostlist.go
+++ hostlist/hostlist.go
@@ -3,6 +3,8 @@ package hostlist
 import "github.com/ocmdev/blacklist/datatypes"
 
 type (
+
+	// Interface for processing blacklist sources
 	HostList interface {
 		UpdateList(c chan datatypes.BlacklistHost) error
 		ValidList(MetaData) bool
@@ -10,6 +12,7 @@ type (
 		MetaData() MetaData
 	}
 
+	// Information about a blacklist source
 	MetaData struct {
 		Name       string
 		Url        string
@@ -22,10 +25,12 @@ var (
 	availableHostLists []HostList
 )
 
+// Return a list of available blacklist sources
 func GetAvailableHostLists() []HostList {
 	return availableHostLists
 }
 
+// Add source to list of blacklist sources
 func AddHostList(newHostList HostList) {
 	availableHostLists = append(availableHostLists, newHostList)
 }
--- hostlist/myipms.go
+++ hostlist/myipms.go
@@ -12,7 +12,10 @@ import (
 	"github.com/ocmdev/blacklist/datatypes"
 )
 
+// URL to retrieve this list
 const MyIpMsUrl = "https://myip.ms/files/blacklist/general/full_blacklist_database.zip"
+
+// Location to download list
 const DownloadLocation = "/tmp/myipms_full.zip"
 
 type (
@@ -20,13 +23,17 @@ type (
 	}
 )
 
+// Download the blacklist file
 func (m *MyIpMs) downloadFile(fname string) error {
+
+	// Create the file to copy data into
 	out, err := os.Create(fname)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
+	// Retrieve the file from the URL
 	resp, err := http.Get(MyIpMsUrl)
 	defer resp.Body.Close()
 
@@ -34,54 +41,70 @@ func (m *MyIpMs) downloadFile(fname string) error {
 		return err
 	}
 
+	// Copy http response into file
 	_, err = io.Copy(out, resp.Body)
 
 	return err
 }
 
+// Read the contents of the downloaded zip file.
 func (m *MyIpMs) readZipFile(fname string, line chan string) error {
+
+	// Open the archive
 	r, err := zip.OpenReader(fname)
 	if err != nil {
 		return err
 	}
 	defer r.Close()
 
+	// Iterate over files in the archive
 	for _, f := range r.File {
+
+		// Open individual file.
 		rc, err := f.Open()
 		if err != nil {
 			return err
 		}
 
+		// Create scanner for file and split into lines.
 		scanner := bufio.NewScanner(rc)
 		scanner.Split(bufio.ScanLines)
 
+		// Iterate over lines in file
 		for scanner.Scan() {
 			text := scanner.Text()
+
+			// Send line into the output chanel
 			line <- text
 		}
 
+		// Close this file.
 		rc.Close()
 	}
 
 	return nil
 }
 
+// Update the list of blacklisted sources.
 func (m *MyIpMs) UpdateList(c chan datatypes.BlacklistHost) error {
+
+	// Download new blacklist file.
 	err := m.downloadFile(DownloadLocation)
 	if err != nil {
 		return err
 	}
 
+	// Create a chanel for reading from the file.
 	line := make(chan string)
 
-	// var wg sync.WaitGroup
-	// wg.Add(1)
+	// Read data from the zip file in a new thread.
 	go func(line chan string) {
 		m.readZipFile(DownloadLocation, line)
-		// wg.Done()
 		close(line)
 	}(line)
 
+	// Obtain and parse lines from the blacklist file.
+	// TODO: Develop this parser / split into function.
 	for l := range line {
 		if len(l) > 0 && l[0] == '#' {
 			continue
@@ -97,25 +120,32 @@ func (m *MyIpMs) UpdateList(c chan datatypes.BlacklistHost) error {
 	return nil
 }
 
+// Check if the metadata passed in is still considered valid.
 func (m *MyIpMs) ValidList(mdata MetaData) bool {
+
+	// Make sure the date last updated is available.
 	if mdata.LastUpdate < 1 {
 		return false
 	}
 
+	// Check the time duration since the last time this file was updated
+	// For this list, older than 8 days is not valid.
 	lastUpdate := time.Unix(mdata.LastUpdate, 0)
 	since := time.Since(lastUpdate)
 	ret := false
-	if since.Hours() < (5 * 24) {
+	if since.Hours() < (8 * 24) {
 		ret = true
 	}
 
 	return ret
 }
 
+// Return the name of this source.
 func (m *MyIpMs) Name() string {
 	return "myip-ms"
 }
 
+// Return meta data about this source (to be stored in database)
 func (m *MyIpMs) MetaData() MetaData {
 	var ret MetaData
 	ret.Name = m.Name()
@@ -124,10 +154,12 @@ func (m *MyIpMs) MetaData() MetaData {
 	return ret
 }
 
+// Initialization
 func init() {
 	AddHostList(NewMyIpMs())
 }
 
+// Return a new instance of this source
 func NewMyIpMs() HostList {
 	return &MyIpMs{}
 }
