00ba311872a65d3a93433f8ac0c635c83a91baf6
Joe <joelillo113@gmail.com>
2017-01-24 22:16:40 -0700

    Comments in blacklist.go

--- blacklist.go
+++ blacklist.go
@@ -1,7 +1,6 @@
 package blacklist
 
 import (
-	"fmt"
 	"log"
 	"sync"
 
@@ -24,29 +23,41 @@ func NewBlackList() *BlackList {
 	}
 }
 
+// Initialize the blacklist database(s)
 func (b *BlackList) Init(host string, port int, dbname string) {
+
+	// Open the database connection
 	err := b.Db.Init(host, port)
 	if err != nil {
 		log.Println("Error opening blacklist database")
 		return
 	}
 
+	// Iterate over all of the blacklist sources
 	for _, val := range b.Sources {
+
+		// Get meta data about this source
 		sourceInfo, _ := b.Db.GetMetaData(val, dbname)
+
+		// Ensure the source is still valid
 		if !val.ValidList(sourceInfo) {
+
+			// Remove all host entries from this source
 			b.Db.RemoveHostList(val, dbname)
 			b.Db.RegisterHostList(val, dbname)
 
+			// Chanel for populating database with new hosts.
 			c := make(chan datatypes.BlacklistHost)
 
+			// Ask this source for an updated list of hosts.
 			var wg1 sync.WaitGroup
 			wg1.Add(1)
-
 			go func() {
 				val.UpdateList(c)
 				wg1.Done()
 			}()
 
+			// Insert hosts from the current source into the database.
 			var wg2 sync.WaitGroup
 			wg2.Add(1)
 			go func() {
@@ -54,14 +65,15 @@ func (b *BlackList) Init(host string, port int, dbname string) {
 				wg2.Done()
 			}()
 
+			// Wait for everything to finish.
 			wg1.Wait()
 			close(c)
 			wg2.Wait()
-
 		}
 	}
 }
 
-func (b *BlackList) CheckHosts(hosts []string, dbname string) {
-	fmt.Println(b.Db.FindHosts(hosts, dbname))
+// Check for hosts in the blacklist database.
+func (b *BlackList) CheckHosts(hosts []string, dbname string) []datatypes.QueryResult {
+	return b.Db.FindHosts(hosts, dbname)
 }
